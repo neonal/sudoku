20b1c264a01e97c3c807368a2eb978c759981404
neonal <onal@woisio.com>
2014-10-23 23:18:47 +0300

    initial commit

---
+++ src/main/java/neonal/SudokuChecker.java
@@ -0,0 +1,95 @@
+package neonal;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Hello world!
+ *
+ */
+public class SudokuChecker {
+
+	private static final int SIZE = 9;
+	private static final int TOTAL_SIZE = 81;
+
+	private static final Path PATH = FileSystems.getDefault().getPath(
+			"resources", "samples.txt");
+	
+	private int[] sqIndexes = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+	public void validate() {
+		try {
+			List<String> allLines = Files.readAllLines(PATH,
+					Charset.forName("UTF-8"));
+
+			int invCount = 0;
+
+
+			for (String string : allLines) {
+				if (string.startsWith("#")) {
+					continue;
+				}
+
+				int[][] rows = new int[SIZE][SIZE];
+				int[][] columns = new int[SIZE][SIZE];
+				int[][] squares = new int[SIZE][SIZE];
+
+				parseString(string, rows, columns, squares);
+
+				boolean valid = true;
+				for (int i = 0; valid && i < SIZE; i++) {
+					valid &= validate(rows[i]) && validate(columns[i])
+							&& validate(squares[i]);
+				}
+
+				if (!valid) {
+					System.out.println(string);
+					invCount++;
+				}
+
+				Arrays.fill(sqIndexes, 0);
+			}
+
+			System.out.println(invCount);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean validate(int[] check) {
+		Arrays.sort(check);
+		for (int i = 0; i < check.length; i++) {
+			if (check[i] != i + 1) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public void parseString(String string, int[][] rows, int[][] columns, int[][] squares){
+		for (int i = 0; i < TOTAL_SIZE; i++) {
+			int number = Character.getNumericValue(string.charAt(i));
+
+			rows[i / 9][i % 9] = number;
+			columns[i % 9][i / 9] = number;
+
+			int sqIndex = ((i / 3) % 3) + ((i / 27) * 3);
+
+			squares[sqIndex][sqIndexes[sqIndex]++] = number;
+		}
+	}
+	
+	public static void main(String[] args) {
+		SudokuChecker sc = new SudokuChecker();
+
+		sc.validate();
+	}
+
+}
---
+++ src/test/java/neonal/AppTest.java
@@ -0,0 +1,90 @@
+package neonal;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	public void testInvalidArray() {
+		int[] invalid = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+		SudokuChecker sc = new SudokuChecker();
+
+		assertFalse(sc.validate(invalid));
+	}
+
+	public void testValidOrderedArray() {
+		int[] invalid = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+		SudokuChecker sc = new SudokuChecker();
+
+		assertTrue(sc.validate(invalid));
+	}
+
+	public void testValidShuffledArray() {
+		int[] invalid = { 9, 1, 6, 2, 8, 3, 4, 7, 5 };
+
+		SudokuChecker sc = new SudokuChecker();
+
+		assertTrue(sc.validate(invalid));
+	}
+
+	public void testParseString() {
+		String s = "123456789123456789123456789123456789123456789123456789123456789123456789123456789";
+
+		int[][] rows = { { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9 } };
+		int[][] columns = { { 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+				{ 2, 2, 2, 2, 2, 2, 2, 2, 2 }, { 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+				{ 4, 4, 4, 4, 4, 4, 4, 4, 4 }, { 5, 5, 5, 5, 5, 5, 5, 5, 5 },
+				{ 6, 6, 6, 6, 6, 6, 6, 6, 6 }, { 7, 7, 7, 7, 7, 7, 7, 7, 7 },
+				{ 8, 8, 8, 8, 8, 8, 8, 8, 8 }, { 9, 9, 9, 9, 9, 9, 9, 9, 9 } };
+		int[][] squares = { { 1, 2, 3, 1, 2, 3, 1, 2, 3 },
+				{ 4, 5, 6, 4, 5, 6, 4, 5, 6 }, { 7, 8, 9, 7, 8, 9, 7, 8, 9 },
+				{ 1, 2, 3, 1, 2, 3, 1, 2, 3 }, { 4, 5, 6, 4, 5, 6, 4, 5, 6 },
+				{ 7, 8, 9, 7, 8, 9, 7, 8, 9 }, { 1, 2, 3, 1, 2, 3, 1, 2, 3 },
+				{ 4, 5, 6, 4, 5, 6, 4, 5, 6 }, { 7, 8, 9, 7, 8, 9, 7, 8, 9 } };
+
+		int[][] parsedRows = new int[9][9];
+		int[][] parsedColumns = new int[9][9];
+		int[][] parsedSquares = new int[9][9];
+
+		SudokuChecker sc = new SudokuChecker();
+
+		sc.parseString(s, parsedRows, parsedColumns, parsedSquares);
+
+		boolean equal = true;
+		for (int i = 0; equal && i < 9; i++) {
+			for (int j = 0; equal && j < 9; j++) {
+				equal &= parsedRows[i][j] == rows[i][j]
+						&& parsedColumns[i][j] == columns[i][j]
+						&& parsedSquares[i][j] == squares[i][j];
+			}
+		}
+
+		assertTrue(equal);
+	}
+}
